f4e8a97322b45b27c5186b51c10178179c0f436b
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-10-30 09:15:19 +0300

    done

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,28 @@
 class Author
   attr_accessor :name
-
+   @@all=[]
 
   def initialize(name)
     @name = name
+    @@all<<self
+  end
+  def self.all
+    @@all
+  end
   
+  def articles
+    Article.all.filter{|article|article.author == self}
+  end
+  def magazines
+    self.articles.map do |article|
+      article.magazine
+    end.uniq
+  end
+  def add_article(title,magazine_name,magazine_category)
+    Article.new(self,title,magazine_name,magazine_category)
+  end
+  def topic_areas
+    self.magazine.map{|magazine|magazine.category}.uniq
   end
 
 
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -2,7 +2,7 @@ class Magazine
   attr_accessor :name, :category
    @@all =[]
    @@count=0
-  def initialize(name, category)
+  def initialize(name:, category:)
     @name = name
     @category = category
      @@count =+1
@@ -15,6 +15,25 @@ class Magazine
   
   def self.all
     @@all
+  end
+
+  def articles
+    Article.all.filter {|article|article.magazine == self}
+  end
+  def articles_titles
+    self.articles.map{|articles|article.title}
+  end
+  def contributors
+    self.articles.map{|article|article.author}.uniq
+  end
+  def contributing_authors
+    self.contibutors.filter{|author|author.articles.count >2}
+  end
+  
+  def self.find_by_name(name)
+    self.all.find do |magazine|
+      magazine.name ==name
+    end
   end
 
 end
